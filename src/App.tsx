ac01ae1c391477833dfa8374ee33589155386ed7
BenjaminMLiestol <bennyliestol@gmail.com>
2024-05-02 17:55:24 +0200

    added biome + husky precommit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
 			</div>
 			<h1>Vite + React</h1>
 			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
+				<button type="button" onClick={() => setCount((count) => count + 1)}>
 					count is {count}
 				</button>
 				<p>
--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,14 @@ import App from "./App.tsx";
 import "./index.css";
 import "./core/i18n";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-);
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+	ReactDOM.createRoot(rootElement).render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>,
+	);
+} else {
+	console.error("Failed to find the root element");
+}
